183d4e80762a19a8e0e5bc1abd23478605d90fa8
Sashi Adhithya <70097892+sashiadhithya@users.noreply.github.com>
2022-07-20 01:36:23 +0530

    Update GRPA3.py

--- week5/GRPA3.py
+++ week5/GRPA3.py
@@ -1,3 +1,15 @@
+'''
+Write a function IsNegativeWeightCyclePresent(WList) that accepts a weighted adjacenty list
+WList for a directed and connected graph and returns True if the graph has a negative weight
+cycle. Otherwise, return False.
+
+Input
+    4
+    [(0,1,10),(0,2,-5),(0,3,2),(3,2,-5),(2,1,-20),(1,3,10)]
+Output
+    True
+'''
+
 def fun(l):
     WL = {}
     for i in range(size):
@@ -65,3 +77,33 @@ def IsNegativeWeightCyclePresent(WL):
             if DA[i] != DB[i]:
                 return True
         return False
+
+# Solution 2
+
+def IsNegativeWeightCyclePresent(WList):
+    s = 0
+    infinity = 1 + len(WList.keys())*max([d for u in WList.keys() for (v,d) in WList[u]])
+    distance = {}
+    for v in WList.keys():
+        distance[v] = infinity        
+    distance[s] = 0 
+    for i in WList.keys():
+        for u in WList.keys():
+            for (v,d) in WList[u]:
+                distance[v] = min(distance[v], distance[u] + d)
+
+    for u in WList.keys():
+        for (v,d) in WList[u]:
+            if (distance[u] + d < distance[v]):
+                return True
+    return False
+
+size = int(input())
+edges = eval(input())
+WL = {}
+for i in range(size):
+    WL[i] = []
+for ed in edges:
+    WL[ed[0]].append((ed[1],ed[2]))
+    
+print(IsNegativeWeightCyclePresent(WL))
