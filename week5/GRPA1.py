8b6fd818b17d600f27bc32187953e261eb1be35d
Sashi Adhithya <70097892+sashiadhithya@users.noreply.github.com>
2022-07-20 01:32:58 +0530

    Update GRPA1.py

--- week5/GRPA1.py
+++ week5/GRPA1.py
@@ -1,3 +1,29 @@
+'''
+An Internet service provider company wants to lay fiber lines to connect in cities labeled 0 to n-1. 
+
+Write a function FiberLink(distance_map) that accepts a weighted adjacenty list
+distance_map in the following format:-
+
+distance_map = {
+
+                source_index : [(destination_index, distance (km)) ,
+                (destination_index, distance) , . .],
+                A W
+
+                source_index : [(destination_index, distance),
+                (destination_index, distance) , . .]
+                
+                }
+
+The function returns the minimum length of fiber required to connect all in cities.
+
+Input
+    7
+    [(0,1,10),(0,2,50),(0,3,300),(5,6,45),(2,1,30),(6,4,37),(1,6,65),(2,5,76),(1,3,40),(3,4,60),(2,4,20)]
+Output
+    182
+'''
+
 def adjL(dedges):
     edges = dedges + [(j,i,w) for (i,j,w) in dedges]
     print(edges)
@@ -56,6 +82,43 @@ def FiberLink(distance_map):
                 s += B[j][2]
     return s
 
+
+size = int(input())
+edges = eval(input())
+WL = {}
+for i in range(size):
+    WL[i] = []
+for ed in edges:
+    WL[ed[0]].append((ed[1],ed[2]))
+    WL[ed[1]].append((ed[0],ed[2]))
+print(FiberLink(WL))
+
+
+# Solution 2
+
+def kruskal(WList):
+    (edges,component,TE)=([],{},[])
+    for u in WList.keys():
+        edges.extend([(d,u,v) for (v,d) in WList[u]])
+        component[u] = u
+    edges.sort()
+    for (d,u,v) in edges:
+        if component[u] != component[v]:
+            TE.append((u,v))
+            c = component[u]
+        for w in WList.keys():
+            if component[w] == c:
+                component[w] = component[v]
+    return(TE)
+
+def FiberLink(distance_map):
+    R = kruskal(distance_map)
+    S = 0
+    for e in R:
+        for ed in distance_map[e[0]]:
+            if ed[0]==e[1]:
+                S += ed[1]
+    return S
 
 size = int(input())
 edges = eval(input())
