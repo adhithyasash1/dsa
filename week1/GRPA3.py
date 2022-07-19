233b3d8c27c3643a3bf7d444846c4be2e75e2cd6
Sashi Adhithya <70097892+sashiadhithya@users.noreply.github.com>
2022-07-20 01:19:46 +0530

    Update GRPA3.py

--- week1/GRPA3.py
+++ week1/GRPA3.py
@@ -1,3 +1,18 @@
+'''
+Write a function named odd_one (L) that accepts a list L as argument. Except for one element,
+all other elements in _ are of the same data type. The function odd_one should return the data
+type of this odd element.
+
+For example, if _ is equal to [1, 2, 3.4, 5, 10] , then the function should return the string
+float. This is because the element 3.4 is the odd one here, all other elements are integers.
+
+Note :
+(1) L has at least three elements.
+(2) For each test case, the elements in the list will only be of one of these four types: int , float, str, bool.
+(3) The function must return one of these four strings: int, float, str, bool.
+(4) Hint: type (1) == int evaluates to True.
+'''
+
 def yo(L):
     a = type(L[0])
     count = len(L)
@@ -21,3 +36,15 @@ def odd_one(L):
     if type(x) == type(True):
         print('bool')
     
+# Solution 2
+
+def odd_one(L):
+  P = {}
+  for elem in L:
+    if type(elem) not in P:
+      P[type(elem)] = 0
+    P[type(elem)] += 1
+  for key, value in P.items():
+    if value == 1:
+      return key.__name__
+print(odd_one(eval(input().strip())))
