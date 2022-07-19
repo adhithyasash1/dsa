0574dae25fb8309b6fd6f42648188f4985372c55
Sashi Adhithya <70097892+sashiadhithya@users.noreply.github.com>
2022-07-20 01:20:46 +0530

    Update GRPA1.py

--- week2/GRPA1.py
+++ week2/GRPA1.py
@@ -1,3 +1,21 @@
+'''
+Write a python function combinationSort(strList) that takes a list of unique strings as argument,
+where each string is a combination of a letter from a to z and a number from 0 to 99, 
+the initial character in string being the letter.
+
+ex : d34, g54, d12, b87, g1, c65, g40, g5, d77
+
+this function should sort the list and return two lists (L1, L2) in the order mentioned below
+
+L1 : First list should contain all strings sorted in ascending order with respect to
+     first character only, all the strings with same initial character should be in the same 
+     order as in the original list.
+
+L2 : In the list L1 above, sort the strings starting with the same character, in descending 
+     with respect to the number formed by the remaining characters.
+'''
+
+
 def combinationSort(L):
     L1 = [ ]
     for i in L:
