6fd9d27d65bf8de77f32677de57fc7a3e31d2413
Sashi Adhithya <70097892+sashiadhithya@users.noreply.github.com>
2022-07-20 01:18:51 +0530

    Update GRPA2.py

--- week1/GRPA2.py
+++ week1/GRPA2.py
@@ -1,3 +1,27 @@
+'''
+Goldbach's conjecture is one of the oldest and best-known unsolved problems in number
+theory. It states that every even number greater than 2 is the sum of two prime numbers.
+For Example:
+12 = 5 +7
+
+26 = 3 + 23 or 7 + 19 or 13 +13
+Write a function Goldbach(n) where in is a positive even number(n > 2 ) that returns a list of
+tuples. In each tuple (a, b) where a <= b, a and b should be prime numbers and the sum of
+
+a and b should be equal to n .
+
+Input:
+    1 12
+Output:
+    [(5,7)]
+
+Input:
+    26
+Output:
+    [(3, 23) , (7, 19) , (13,13) ]
+'''
+
+
 def factors(n):
     ans = [ ]
     for i in range(1, n+1):
@@ -24,6 +48,8 @@ def goldbach(z):
                 if (j,i) not in res:
                     res.append((i,j))
     return res
+                
+    
                 
     
 
