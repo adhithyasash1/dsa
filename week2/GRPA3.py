5d921477fe4a5d316788e0205924556d3cb994c6
Sashi Adhithya <70097892+sashiadhithya@users.noreply.github.com>
2022-07-20 01:22:43 +0530

    Update GRPA3.py

--- week2/GRPA3.py
+++ week2/GRPA3.py
@@ -1,3 +1,28 @@
+'''
+Merging two sorted arrays in place
+
+Given a custom implementation of list named MyList.
+On MyList objects you can perform read operations similar to the in build 
+lists in python, example use A[i] to read element in index i in MyList object A.
+
+The only possible operation that you can use to edit data in MyList objects is
+by calling the swap method. For instance, A.swap(indexA, B, indexB) will swap values
+at A[indexA] and B[indexB], and A.swap(index1, A, index2) will swap A[index1] and A[index2],
+where indexA, indexB, index1, index2 are all integers.
+
+Complete the python function mergeInPlace(A, B) that accepts two MyLists A and B containing
+integers that are sorted in ascending order and merges them in place (without using any other list)
+such that after merging, A and B are still sorted in ascending order with the smallest 
+element of both MyLists as the first element of A.
+
+Input : 
+	2 4 6 9 13 15
+	1 3 5 10
+Output :
+	[1,2,3,4,5,6]
+	[9,10,13,15]
+'''
+
 def mergeInPlace(A, B):
 	a, b = len(A), len(B)
 	if a > b:
@@ -37,7 +62,8 @@ def mergeInPlace(A, B):
 		return(A, B)
 
 
-'''
+# Solution 2
+
 def merge(A, B):
     m, n = len(A), len(B)
     C, i, j, k = [], 0, 0, 0
@@ -72,6 +98,3 @@ def mergeInPlace(A, B):
             ans2.append(C[j])
     if ans1 != B and ans2 !=A: 
         return ans1, ans2
-'''
-	
-
