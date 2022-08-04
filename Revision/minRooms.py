8369c5c52e5b43d55f749eec8d702450f9b0de72
Sashi Adhithya <70097892+adhithyasash1@users.noreply.github.com>
2022-08-04 09:03:40 +0530

    Update minRooms.py

--- Revision/minRooms.py
+++ Revision/minRooms.py
@@ -1,1 +1,27 @@
+from heapq import *
 
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __lt__(self, other):
+        self.end < other.end
+
+def minMeetingRoom(intervals):
+    intervals.sort(key=lambda x : x.start)
+
+    rooms = 0
+    minHeap = []
+
+    for interval in intervals:
+        # remove all the meetings that have ended
+        while len(minHeap) > 0 and interval.start >= minHeap[0].end:
+            # remove
+            heappop(minHeap)
+
+        heappush(minHeap, interval)
+
+        rooms = max(rooms, len(minHeap))
+
+    return rooms
